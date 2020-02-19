de584fa7d651cec3fa786b27eaf45fe0818cb6f5
chaleto <milev981@abv.bg>
2020-02-19 20:45:40 +0200

    this is simple function

---
+++ test5.php
@@ -0,0 +1,20 @@
+<?php
+class sel{
+    public function __construct($table,$id){
+        $this->table = $table;
+        $this->id = $id;
+    $sql = "SELECT ${id} FROM ${table}";
+    $selected = $conn->prepare($sql);
+    $selected->bindParam('id',$id,PDO::PARAM_STR);
+    $selected->bindParam('table',$table,PDO::PARAM_STR);
+    $selected->execute();
+
+    while($row = $selected->fetch(PDO::FETCH_ASSOC)){
+        echo $row['ID'];
+    }
+    }
+}
+$nc = new sel('users','user_name');
+
+
+?>
